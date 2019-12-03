8acb8170a287286f2536cf23e94b0e03a6e0c3d7
Yuna Seol <yunaseol@kaist.ac.kr>
2019-12-03 22:39:18 +0900

    Fix flags in eval

--- eval.py
+++ eval.py
@@ -9,6 +9,7 @@ import data_helpers
 from text_cnn import TextCNN
 from tensorflow.contrib import learn
 import csv
+import sys
 
 # Parameters
 # ==================================================
@@ -28,7 +29,8 @@ tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on 
 
 
 FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
+#FLAGS._parse_flags()
+FLAGS(sys.argv)
 print("\nParameters:")
 for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.upper(), value))
