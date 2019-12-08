125adbaf0834953f020c7b2f98b2a556d6925b34
Yuna Seol <yunaseol@kaist.ac.kr>
2019-12-08 16:26:20 +0900

    Implementing cnn-multichannel

--- text_cnn.py
+++ text_cnn.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.keras.applications import ResNet50
 
 
 class TextCNN(object):
@@ -9,10 +10,10 @@ class TextCNN(object):
     """
     def __init__(
       self, sequence_length, num_classes, vocab_size,
-      embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+      embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0, model="cnn", num_channels=1):
 
         # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_x = tf.placeholder(tf.int32, [None, num_channels, sequence_length], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
@@ -25,15 +26,32 @@ class TextCNN(object):
                 tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
                 name="W",
                 trainable=True)
-            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x[:,0,:])
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+            # print("original: ",self.embedded_chars_expanded.shape)
+            # (?, 52, 300, 1)
+            if num_channels == 2:
+                W2 = tf.Variable(
+                    tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                    name="W2",
+                    trainable=False)
+                emb2 = tf.nn.embedding_lookup(W2, self.input_x[:,1,:])
+                self.embedded_chars_expanded = tf.stack([self.embedded_chars, emb2], axis=3)
+                # print("multichannel: ",self.embedded_chars_expanded.shape)
+                # (?, 52, 300, 2)
+
+        #change model into resnet
+        if model == "resnet":
+            model = ResNet50(include_top=True, weights=None, pooling='max', classes=num_classes,\
+                input_tensor=self.embedded_chars_expanded)
+            return model
 
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
         for i, filter_size in enumerate(filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
                 # Convolution Layer
-                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                filter_shape = [filter_size, embedding_size, num_channels, num_filters]
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
                 b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
                 conv = tf.nn.conv2d(
@@ -54,7 +72,7 @@ class TextCNN(object):
                 pooled_outputs.append(pooled)
 
         # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
+        num_filters_total = num_filters * len(filter_sizes) # 3 * 128 = 384
         self.h_pool = tf.concat(pooled_outputs, 3)
         self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
--- train.py
+++ train.py
@@ -13,13 +13,14 @@ from tensorflow.contrib import learn
 # ==================================================
 
 # Data loading params
-tf.flags.DEFINE_string("checkpoint_directory","w2v_nonstatic", "Name of checkpointdirectory")
+tf.flags.DEFINE_string("checkpoint_directory","w2v_multichannel", "Name of checkpoint directory")
 
 # Model Hyperparameters
 tf.flags.DEFINE_string("embedding", "word2vec", "Pretrained word embedding to use (default: None)")
 tf.flags.DEFINE_integer("embedding_dim", 300, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
+tf.flags.DEFINE_integer("num_channels", 2, "Number of channels (default: 1)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
 
@@ -90,7 +91,8 @@ def train(x_train, y_train, vocab_processor, x_dev, y_dev):
                 embedding_size=FLAGS.embedding_dim,
                 filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
                 num_filters=FLAGS.num_filters,
-                l2_reg_lambda=FLAGS.l2_reg_lambda)
+                l2_reg_lambda=FLAGS.l2_reg_lambda,
+                num_channels=FLAGS.num_channels)
 
             # Define Training procedure
             global_step = tf.Variable(0, name="global_step", trainable=False)
@@ -161,7 +163,7 @@ def train(x_train, y_train, vocab_processor, x_dev, y_dev):
                                 word.append(ch)
                             else:
                                 print('else: ', word, ch)
-                        print(word)
+                        # print(word)
                         idx = vocab_processor.vocabulary_.get(word)
                         # print("value of idx is" + str(idx));
                         if idx != 0:
@@ -178,7 +180,7 @@ def train(x_train, y_train, vocab_processor, x_dev, y_dev):
                 A single training step
                 """
                 feed_dict = {
-                  cnn.input_x: x_batch,
+                  cnn.input_x: np.reshape(x_batch, [-1, FLAGS.num_channels, x_train.shape[1]]),
                   cnn.input_y: y_batch,
                   cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
                 }
@@ -194,7 +196,7 @@ def train(x_train, y_train, vocab_processor, x_dev, y_dev):
                 Evaluates model on a dev set
                 """
                 feed_dict = {
-                  cnn.input_x: x_batch,
+                  cnn.input_x: np.reshape(x_batch, [-1, FLAGS.num_channels, x_train.shape[1]]),
                   cnn.input_y: y_batch,
                   cnn.dropout_keep_prob: 1.0
                 }
